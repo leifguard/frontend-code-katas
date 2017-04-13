56fd192ae06ab1c64dea8170357672cbab2c7567
Leif Arne Genzmer <l.genzmer@neusta.de>
2017-04-13 16:49:54 +0200

    remove solution

--- javascript/test/fizzbuzz.js
+++ javascript/test/fizzbuzz.js
@@ -1,16 +1,5 @@
 function fizzbuzz(input) {
 	
-	var output = "";
-	
-	if(input % 3 == 0) {
-		output = "fizz";
-	} 
-	
-	if(input % 5 == 0) {
-		output += "buzz";
-	} 
-	
-	return (output == "") ? input : output;
 	
 }
 
